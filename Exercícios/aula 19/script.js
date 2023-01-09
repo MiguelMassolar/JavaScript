e58b218b7febc74a9ec1738984903503c875c611
MiguelMassolar <114073783+MiguelMassolar@users.noreply.github.com>
2023-01-09 15:53:43 -0300

    Prosseguindo com os estudos de JS

---
+++ Exercícios/aula 19/script.js
@@ -0,0 +1,16 @@
+/*function calcularAreaTerreno(largura, comprimento){
+    let area = largura * comprimento
+    return area
+}
+
+let largura = prompt('Digite a largura do terreno em metros')
+let comprimento = prompt('Digite o comprimento do terreno em metros')
+let area = calcularAreaTerreno(largura, comprimento)
+
+document.write('O terreno possui ' + area + ' metros quadrados.')*/
+
+function soma(a, b){
+    return a + b
+}
+
+console.log(soma(7, 7))
